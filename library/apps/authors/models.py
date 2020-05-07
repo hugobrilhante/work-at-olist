2ee06e299808bf814ae68e6be93b918725f78f34
Hugo Brilhante <hugobrilhante@gmail.com>
2020-05-06 23:12:05 -0300

    feat(authors): add model authors

--- library/apps/authors/models.py
+++ library/apps/authors/models.py
@@ -1,2 +1,12 @@
+from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        verbose_name = 'author'
+        verbose_name_plural = 'authors'
+
+    def __str__(self):
+        return self.name
