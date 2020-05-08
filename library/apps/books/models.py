32407f35b1c895dd233a33719497eeb7416c97ac
Hugo Brilhante <hugobrilhante@gmail.com>
2020-05-08 10:31:55 -0300

    feat(books): add model book

--- library/apps/books/models.py
+++ library/apps/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    edition = models.CharField(max_length=100, blank=True)
+    publication_year = models.DateTimeField(auto_now=timezone.now)
+    authors = models.ManyToManyField(
+        'authors.Author',
+        related_name='authors'
+    )
+
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
+
+    def __str__(self):
+        return f'{self.name} - Edition: {self.edition}'
